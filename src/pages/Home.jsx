d770a4d3a40727b8874bcbe057eb6ea96340cd05
DhineshiniArumugam <dhineshini_arumugam@persistent.com>
2023-10-30 10:38:57 -0400

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React,{userEffect,userState} from 'react'
+import {Container,Button} from 'react-bootstrap'
+import '../styles/Home.css'
+import axios from "axios";
+import API_URL from "../config/global";
+
+const Home = () => {
+  const [res,setRes] = useState({})
+  useEffect(()=>{
+    const user=JSON.parse(localStorage.getItem("userInfo"))
+    if(user && user.token){
+      getData(user.token)
+    }
+  },[])
+
+
+const getData= async(token)=>{
+    try{
+      const config = {
+        headers:{Authorization:token}
+      }
+      const response = await axios.post(`${API_URL}/home`,config)
+      console.log(response);
+      if(response.data==="Invalid Token"){
+        alert("login again")
+      }else if(response.data === "Server Busy"){
+        alert("unauthorized access")
+      }else if(response?.status){
+        setRes(response.data)
+      }
+    }catch(e){
+      console.log(e)
+    }
+  }
+  return (
+    <Container>
+        <h1>Welcome to our WebPage</h1>
+        <p>We are here to serve you</p>
+        <p>{res.name}</p>
+        <Button variants="primary" type="submit">Get started</Button>
+    </Container>
+  )
+}
+
+export default Home
